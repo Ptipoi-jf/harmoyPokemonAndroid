05893d22f5942e0ff7039a32b107aaa5f70f7637
Florian Jarnoin <florian.jarnoin@outlook.fr>
2016-05-27 21:01:49 +0200

    initial commit default android project

---
+++ src/com/kerhomjarnoin/pokemon/PokemonApplication.java
@@ -0,0 +1,27 @@
+/**************************************************************************
+ * PokemonApplication.java, pokemon Android
+ *
+ * Copyright 2016
+ * Description : 
+ * Author(s)   : Harmony
+ * Licence     : 
+ * Last update : May 27, 2016
+ *
+ **************************************************************************/
+package com.kerhomjarnoin.pokemon;
+
+/**
+ * Custom Pokemon Application context.
+ *
+ * Feel free to modify this class.
+ */
+public class PokemonApplication extends PokemonApplicationBase {
+    // Override or Create your custom method for your application
+    // this file is just generate at first time, and never override...
+
+    // on release mode use BuildConfig.DEBUG;
+    /**
+     * DEBUG.
+     */
+    public static final boolean DEBUG = true;
+}
